0c9581d3a579cfc69851fe78ef1197d0667d00c1
Egor Budnikov <budnikov16.08@ya.ru>
2025-07-26 17:18:14 +0800

    5

--- yatube_api/posts/constants.py
+++ yatube_api/posts/constants.py
@@ -1,6 +1,5 @@
-from http.client import FORBIDDEN
 from rest_framework import status
 
-FORBIDDEN_403 = status.HTTP_403_FORBIDDEN
 COMMENT_NOT_AUTHOR = {'detail': 'Вы не автор этого комментария.'}
+FORBIDDEN_403 = status.HTTP_403_FORBIDDEN
 POST_NOT_AUTHOR = {"detail": "Вы не автор этого поста."}
