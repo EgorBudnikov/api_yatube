2cbc9456b6b61c85d4030273433761a81fcc31e8
Egor Budnikov <budnikov16.08@ya.ru>
2025-07-26 18:53:00 +0800

    7

--- yatube_api/posts/urls.py
+++ yatube_api/posts/urls.py
@@ -1,22 +1,22 @@
-# # from django.urls import include, path
-# from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-# from .views import CommentAPIView, CommentListCreate, GroupViewSet, PostViewSet
+from .views import CommentAPIView, CommentListCreate, GroupViewSet, PostViewSet
 
-# router = DefaultRouter()
-# router.register(r'posts', PostViewSet)
-# router.register(r'groups', GroupViewSet)
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'groups', GroupViewSet)
 
-# urlpatterns = [
-#     path(
-#         'posts/<int:post_id>/comments/',
-#         CommentListCreate.as_view(),
-#         name='post-comment-list-create'
-#     ),
-#     path(
-#         'posts/<int:post_id>/comments/<int:comment_id>/',
-#         CommentAPIView.as_view(),
-#         name='comment-create-get-gelete-putch'
-#     ),
-#     path('', include(router.urls)),
-# ]
+urlpatterns = [
+    path(
+        'posts/<int:post_id>/comments/',
+        CommentListCreate.as_view(),
+        name='post-comment-list-create'
+    ),
+    path(
+        'posts/<int:post_id>/comments/<int:comment_id>/',
+        CommentAPIView.as_view(),
+        name='comment-create-get-gelete-putch'
+    ),
+    path('', include(router.urls)),
+]
