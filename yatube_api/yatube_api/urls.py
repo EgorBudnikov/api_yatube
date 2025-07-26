f21010bbfb640ca6423e23501d3d4333b20aef5b
Egor Budnikov <budnikov16.08@ya.ru>
2025-07-26 17:24:33 +0800

    6

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,21 +1,21 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-# from django.urls import include, path
-from rest_framework.authtoken import views
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib import admin
+# # from django.urls import include, path
+# from rest_framework.authtoken import views
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
-    path('api/v1/', include('posts.urls'))
-]
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/v1/api-token-auth/', views.obtain_auth_token),
+#     path('api/v1/', include('posts.urls'))
+# ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+#     )
+#     urlpatterns += static(
+#         settings.STATIC_URL, document_root=settings.STATIC_ROOT
+#     )
